e2897cc6accd937b883655994e752fad586e94a1
Will Weston <Williamweston81@gmail.com>
2023-08-04 11:29:50 -0500

    Weston Conflict 3.0

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -3,37 +3,7 @@ require "csv"
 require "pry"
 
 class SeasonStats < StatDaddy
-<<<<<<< HEAD
-  
-    def winningest_coach
-      coach_wins = Hash.new(0)
-      coach_games = Hash.new(0)
-  
-      @games_teams_fixture.each do |data|
-        coach = data[:head_coach]
-        result = data[:result]
-  
-        if result == "WIN" || result == "OT"
-          coach_wins[coach] += 1
-        end
-  
-        coach_games[coach] += 1
-      end
-  
-      highest_win_percentage = 0.0
-      winningest_coach = nil
-  
-      coach_wins.each do |coach, wins|
-        win_percentage = wins.to_f / coach_games[coach]
-        if win_percentage > highest_win_percentage
-          highest_win_percentage = win_percentage
-          winningest_coach = coach
-        end
-      end
-  require 'pry' ; binding.pry
-      winningest_coach
-    end 
-=======
+
   def winningest_coach(season)
     coach_wins = Hash.new(0)
     coach_games = Hash.new(0)
@@ -57,7 +27,6 @@ class SeasonStats < StatDaddy
     winningest_coach = coach_win_percentages.max_by { |_, percentage| percentage }
     winningest_coach.first
   end
->>>>>>> e7745e543c5c0a4fbeaaae08edc006efe1d10e41
 
   def worst_coach(season)
     coach_wins = Hash.new(0)
